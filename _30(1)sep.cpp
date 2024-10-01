7794ccabe8084a9d44e754f9635674a468deede9
SAI SARVESH R <ssr1032005@gmail.com>
2024-10-01 19:49:15 +0530

    edits

--- _30(1)sep.cpp
+++ _30(1)sep.cpp
@@ -25,8 +25,8 @@ complex add(complex a,complex b)
 {
     int r1=a.get_r();
     int i1=a.get_i();
-    int r2=a.get_r();
-    int i2=a.get_i();
+    int r2=b.get_r();
+    int i2=b.get_i();
     int R=r1+r2;
     int I=i1+i2;
     complex c(R,I);
