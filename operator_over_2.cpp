cac488f47438aa71a688f2d6374f5bf3c9efb91a
SAI SARVESH R <ssr1032005@gmail.com>
2024-10-14 12:13:28 +0530

    14th oct new programs c++

---
+++ operator_over_2.cpp
@@ -0,0 +1,38 @@
+// program to add longitude and latitude and storing in a varible LOC  
+#include<iostream>
+using namespace std;
+class sample
+{
+    int lon,lat;
+    public: sample()
+            {
+                lon=lat=0;
+            }
+            sample(int a,int b)
+            {
+                lon=a;
+                lat=b;
+            }
+            void print()
+            {
+                cout<<"latitude"<<lat<<"\n
+                "<<"longitude"<<lon<<endl;
+            }
+            // operator overloading function
+            sample operator+(sample &c)
+            {
+                sample t;
+                t.lon=lon+c.lon;
+                t.lat=lat+c.lat;
+                return t;
+            }
+};
+int main()
+{
+    sample c1(2,4);
+    sample c2(2,3);
+    sample c3;
+    c3=c1+c2;
+    c3.print();
+    return 0;
+}
