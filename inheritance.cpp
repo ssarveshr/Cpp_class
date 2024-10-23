9ce9e07a0cf90d94bf46a2501f72c4d4ada25444
SAI SARVESH R <ssr1032005@gmail.com>
2024-10-23 13:07:36 +0530

    23rd oct 2024

---
+++ inheritance.cpp
@@ -0,0 +1,48 @@
+/*we can creat a gernal class that defines proeprties common to a set of related items
+  class that is inherited is class the base class and a class that is inheriting the
+  members of another class is called the derivied class
+  BASE CLASS access controll
+  class derived_class_name : access base_class name
+  {
+   code 
+   } 
+  the base class access specifier must be either public private or protroted if no 
+  access specifier is present the defualt will be private for the class
+  */
+
+#include<iostream>
+using namespace std;
+class base
+{
+    int i,j;
+    public: void set(int a,int b)
+            {
+                i=a;
+                j=b;
+            }
+            void show()
+            {
+                cout<<i<<endl;
+                cout<<j<<endl;
+            }
+};
+class derived:public base
+{
+    int k;
+    public: derived(int x)
+            {
+                k=x;
+            }
+            void showk()
+            {
+                cout<<k<<endl;
+            }
+};
+int main()
+{
+    derived ob(3);
+    ob.set(1,2);
+    ob.show();
+    ob.showk();
+    return 0;
+}
---
+++ operator_over_6.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<stdlib.h>
+#include<windows.h>
+using namespace std;
+class loc
+{
+    int l,i;
+    public:loc()
+    {}
+    loc(int p,int q)
+    {
+        l=p;
+        i=q;
+    }
+    void show()
+    {
+        cout<<l<<" + "<<i<<endl;
+    }
+    void *operator new(size_t size);
+    void operator delete(void *p);
+};
+void* loc::operator new(size_t size)
+{
+    void *p;
+    cout<<"In overloaded new \n";
+    p=malloc(size);
+    if(!p)
+    {
+        cout<<"Memory allocated failed"<<endl;
+        exit(0);
+    }
+    return p;
+}
+void loc::operator delete(void *p)
+{
+    cout<<"In overloaded delete \n";
+    free(p);
+}
+int main()
+{
+    loc *p1, *p2;
+    p1=new loc(10,20);
+    p2=new loc(30,40);
+    p1->show();
+    p2->show();
+    delete p1;
+    delete p2;
+    // system("cd ..");
+    // system("python");
+    SYSTEMTIME d;
+    GetSystemTime(&d);
+    cout<<d.wYear;
+
+}
+/*new and delete are specific class the use of the operator on any other type of data causes the
+ *original new or delete to be called 
+ *New and delete operators can also be overloaded globally outside of any class any declaration 
+   */
